8dac3c00458940f7a6c3820afbb9dd593a605005
kaizhong <chaikaizhong2021@163.com>
2023-03-30 20:19:14 +0800

    add fast_scnn_stdc backbone

--- configs/_base_/models/fast_scnn_stdc.py
+++ configs/_base_/models/fast_scnn_stdc.py
@@ -4,6 +4,16 @@ model = dict(
     data_preprocessor=data_preprocessor,
     backbone=dict(
         _delete_=True,
-        type = 'STDCContextPathNet', 
+        type = 'STDCContextPathNet',
+        backbone_cfg=dict(
+            type='STDCNet',
+            stdc_type='STDCNet1',
+            in_channels=3,
+            channels=(32, 64, 256, 512, 1024),
+            bottleneck_type='cat',
+            num_convs=4,
+            norm_cfg=norm_cfg,
+            act_cfg=dict(type='ReLU'),
+            with_final_conv=False),
     )
 )
