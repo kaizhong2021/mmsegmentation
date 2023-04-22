ddbf4c4247cd87eb458dd9d0bae8f44ef9da1034
kaizhong2021 <chaikaizhong2021@163.com>
2023-04-22 21:09:41 +0800

    add ddrnet

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -32,6 +32,8 @@ from .setr_mla_head import SETRMLAHead
 from .setr_up_head import SETRUPHead
 from .stdc_head import STDCHead
 from .uper_head import UPerHead
+from .ddr_head import DDRHead
+
 
 __all__ = [
     'FCNHead', 'PSPHead', 'ASPPHead', 'PSAHead', 'NLHead', 'GCHead', 'CCHead',
@@ -41,5 +43,5 @@ __all__ = [
     'SETRMLAHead', 'DPTHead', 'SETRMLAHead', 'SegmenterMaskTransformerHead',
     'SegformerHead', 'ISAHead', 'STDCHead', 'IterativeDecodeHead',
     'KernelUpdateHead', 'KernelUpdator', 'MaskFormerHead', 'Mask2FormerHead',
-    'LightHamHead', 'PIDHead'
+    'LightHamHead', 'PIDHead', 'DDRHead'
 ]
