01f5cfff0ac0ee6496d73794d0cb66624b0f5903
kaizhong2021 <chaikaizhong2021@163.com>
2023-05-01 09:51:02 +0800

    add stdc2_pplitehead config file

--- configs/_base_/models/stdc_ppliteseg.py
+++ configs/_base_/models/stdc_ppliteseg.py
@@ -28,7 +28,6 @@ model = dict(
     decode_head=dict(
         type='PP_LiteSeg_DecodeHead',
         in_channels=256,
-        channels=256,
         num_classes=19),
     auxiliary_head=[
         dict(
