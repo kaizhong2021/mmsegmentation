892313b733bee31d9c5bfb93b5983f3255257e05
kaizhong2021 <chaikaizhong2021@163.com>
2023-05-01 09:45:58 +0800

    add stdc2_pplitehead config file

--- configs/stdc/stdc2_4xb12-80k_cityscapes-512x1024_ppliteseg.py
+++ configs/stdc/stdc2_4xb12-80k_cityscapes-512x1024_ppliteseg.py
@@ -1,2 +1,21 @@
-_base_ = './stdc2_4xb12-80k_cityscapes-512x1024.py'
-model = dict(backbone=dict(backbone_cfg=dict(stdc_type='STDCNet2')))
+_base_ = [
+    '../_base_/models/stdc_ppliteseg.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+crop_size = (512, 1024)
+data_preprocessor = dict(size=crop_size)
+model = dict(data_preprocessor=data_preprocessor)
+param_scheduler = [
+    dict(type='LinearLR', by_epoch=False, start_factor=0.1, begin=0, end=1000),
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=1000,
+        end=80000,
+        by_epoch=False,
+    )
+]
+train_dataloader = dict(batch_size=12, num_workers=4)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
